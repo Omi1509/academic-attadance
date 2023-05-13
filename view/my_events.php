a13c0cdcd212915eb4f251b3dea36767ad20f416
omkar navale <omkarnavale20@gmail.com>
2023-05-14 01:05:26 +0530

    changes in my events

--- view/my_events.php
+++ view/my_events.php
@@ -1,6 +1,6 @@
 <?php
 if(!isset($_SERVER['HTTP_REFERER'])){
-    // redirect them to your desired location
+    // redirect them  desired location
     header('location:../index.php');
     exit;
 }
